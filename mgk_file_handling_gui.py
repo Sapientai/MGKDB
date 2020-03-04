a55a6fb2f857214f859850677b8876ffc6843b5c
Dongyang Kuang <dykuangii@gmail.com>
2020-03-04 13:17:32 -0600

    Switch the order of checking nonlin/linear runs.

--- mgk_file_handling_gui.py
+++ mgk_file_handling_gui.py
@@ -488,14 +488,17 @@ def isLinear(folder_name):
         pars = par.pardict
         linear = not pars['nonlinear']
         return(linear)
-    #check folder name for linear
-    elif folder_name.find('linear') != -1:
-        linear = True 
-        return(linear)
+        
     #check folder name for nonlin
     elif folder_name.find('nonlin') != -1:
         linear = False
         return(linear)
+        
+    #check folder name for linear
+    elif folder_name.find('linear') != -1:
+        linear = True 
+        return(linear)
+
     else:
         assert linear is None, "Can not decide, please include linear/nonlinear as the suffix of your data folder!"
         
